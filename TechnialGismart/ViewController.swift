dc0dc867b7c0bc483311619a893cbb8e9bcd6a63
Egor Evseenko <egorevseenko@inbox.ru>
2022-07-07 00:34:58 +0300

    Initial Commit

---
+++ TechnialGismart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechnialGismart
+//
+//  Created by Егор Евсеенко on 7.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
